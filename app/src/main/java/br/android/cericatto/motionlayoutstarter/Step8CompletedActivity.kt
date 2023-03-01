7182f994626f423fa9fd7efdb78904f6ed248823
Rodrigo Cericatto Konzen <graffiti75@gmail.com>
2023-03-01 15:17:46 -0500

    Adding code from section "4.7.14. (5:17) Running motion with code"

--- app/src/main/java/br/android/cericatto/motionlayoutstarter/Step8CompletedActivity.kt
+++ app/src/main/java/br/android/cericatto/motionlayoutstarter/Step8CompletedActivity.kt
@@ -22,22 +22,21 @@ import com.google.android.material.appbar.AppBarLayout
 
 class Step8CompletedActivity : AppCompatActivity() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_step8_completed)
+	override fun onCreate(savedInstanceState: Bundle?) {
+		super.onCreate(savedInstanceState)
+		setContentView(R.layout.activity_step8_completed)
 
-        coordinateMotion()
-    }
+		coordinateMotion()
+	}
 
-    private fun coordinateMotion() {
-        val appBarLayout: AppBarLayout = findViewById(R.id.appbar_layout)
-        val motionLayout: MotionLayout = findViewById(R.id.motion_layout)
+	private fun coordinateMotion() {
+		val appBarLayout: AppBarLayout = findViewById(R.id.appbar_layout)
+		val motionLayout: MotionLayout = findViewById(R.id.motion_layout)
 
-        val listener = AppBarLayout.OnOffsetChangedListener { unused, verticalOffset ->
-            val seekPosition = -verticalOffset / appBarLayout.totalScrollRange.toFloat()
-            motionLayout.progress = seekPosition
-        }
-
-        appBarLayout.addOnOffsetChangedListener(listener)
-    }
+		val listener = AppBarLayout.OnOffsetChangedListener { unused, verticalOffset ->
+			val seekPosition = -verticalOffset / appBarLayout.totalScrollRange.toFloat()
+			motionLayout.progress = seekPosition
+		}
+		appBarLayout.addOnOffsetChangedListener(listener)
+	}
 }
